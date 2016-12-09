3924950f75d40fa7f76c7008c595d752ddb5e335
rogerbf <roger@bergfalk.nu>
2016-12-09 20:31:39 +0100

    separate error throwing module

--- source/index.js
+++ source/index.js
@@ -1,4 +1,5 @@
 import core from './library/core'
+import throwError from './library/throwError'
 
 export default (configuration = {}) => {
   const { port = undefined, hashedControlPassword = undefined } = (
@@ -7,7 +8,7 @@ export default (configuration = {}) => {
     : configuration
   )
 
-  port === undefined && (() => { throw Error(`a port has to be defined`) })()
+  port === undefined && throwError(Error(`a port has to be defined`))
 
   return core({ state: { port, hashedControlPassword } })
 }
