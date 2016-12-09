1cce4f93600e794865a24d5e5d7c7fb775dcf392
rogerbf <roger@bergfalk.nu>
2016-12-09 20:30:25 +0100

    removed old test

--- source/__test__/index.test.js
+++ source/__test__/index.test.js
@@ -6,15 +6,6 @@ it(`throws`, () => {
   expect(() => commander()).toThrowError(Error(`a port has to be defined`))
 })
 
-// it(`returns expected output`, () => {
-//   expect(commander(9050))
-//     .toEqual({ port: 9050, hashedControlPassword: undefined })
-//   expect(commander({ port: 9055 }))
-//     .toEqual({ port: 9055, hashedControlPassword: undefined })
-//   expect(commander({ port: 9050, hashedControlPassword: `FF00` }))
-//     .toEqual({ port: 9050, hashedControlPassword: `FF00` })
-// })
-
 it(`returns a function`, () => {
   expect(typeof (commander(9050))).toEqual(`function`)
 })
