798b3ee2c437bbfca5e4dab0d269d868450622b5
rogerbf <roger@bergfalk.nu>
2016-12-10 14:38:59 +0100

    inject core

--- source/library/core.js
+++ source/library/core.js
@@ -2,12 +2,12 @@ const core = (configuration = {}) => {
   const { dependencies = [], state = {} } = configuration
 
   return Object.assign(
-    // () => core([ ...dependencies ], { ...state }),
     () => ({ getCurrentState: () => state }),
     dependencies.reduce((methods, fn) => {
       return {
         ...methods,
-        [fn.name]: (...args) => core([ ...dependencies ], fn(state, ...args))
+        // [fn.name]: (...args) => core([ ...dependencies ], fn(state, ...args))
+        [fn.name]: (...args) => core([ ...dependencies ], fn(core, ...args))
       }
     }, {})
   )
