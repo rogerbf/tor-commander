13ed92ac43810ef8bd9172b32e067dc4bd98c2eb
rogerbf <roger@bergfalk.nu>
2016-12-22 23:14:20 +0100

    chaining

--- source/library/send.test.js
+++ source/library/send.test.js
@@ -13,11 +13,22 @@ test(`it returns expected output with no initial state`, () => {
   expect(sender(`SIGNAL NEWNYM`)).toEqual({ queue: [ `SIGNAL NEWNYM` ] })
 })
 
-it(`it returns expected output with initial state`, () => {
+test(`it returns expected output with initial state`, () => {
   const state = { queue: [ `HEARTBEAT` ] }
   const Commander = jest.fn(state => state)
   const sender = send({ Commander, state })
 
   expect(sender(`SIGNAL NEWNYM`))
     .toEqual({ queue: [ `HEARTBEAT`, `SIGNAL NEWNYM` ] })
+})
+
+test(`it modifies the state the expected way`, () => {
+  const Commander = jest.fn(state => ({
+    state,
+    send: send({ Commander, state })
+  }))
+
+  const currentState = Commander({}).send(`hello`).send(`there`).state
+
+  expect(currentState).toEqual({ queue: [ `hello`, `there` ] })
 })
