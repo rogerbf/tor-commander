0990fb9e07870b49e643ae807e6c559fe62edaee
rogerbf <roger@bergfalk.nu>
2016-12-22 23:14:56 +0100

    convert chunk to string

--- source/library/execute.js
+++ source/library/execute.js
@@ -10,8 +10,9 @@ export default ({ dependencies = defaultDependencies, state }) => () => {
   const controlPort = dependencies.socketExec({ port: state.port })
   return new Promise((resolve, reject) => {
     controlPort(state.queue)
-      .then(parseOutput)
+      .then(output => parseOutput(output.toString()))
       .then(combineOutput)
       .then(output => output.success ? resolve(output.data) : reject(`Something went wrong`))
+      .catch(error => reject(error))
   })
 }
